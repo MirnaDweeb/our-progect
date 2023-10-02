a355a418c24e63b74a8af22234557aa3594d1c9f
shosh0 <smt5557755@gmail.com>
2023-10-02 17:52:23 +0300

    shimaa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 //import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+// import your icons
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+
 import Contact from './pages/Contact';
 
 import Skills from './pages/Skills';
@@ -34,3 +41,5 @@ function App() {
 }
 
 export default App;
+library.add(fab, fas, far)
+
