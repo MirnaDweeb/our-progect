fc4dba3a15ffcf2be3ab2416ad0a382ec10810ab
shosh0 <smt5557755@gmail.com>
2023-10-02 17:29:12 +0300

    shimaa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import Contact from './pages/Contact';
+
 import Skills from './pages/Skills';
 import './pages/skills.css'
 
+import Resume from './pages/Resume'; 
+import './pages/Resume.css'
+
+
+import {BrowserRouter , Routes , Route } from 'react-router-dom'
+
 function App() {
   return (
-   <Contact/>
+    <BrowserRouter>
+      
+      <div className="App">
+      {/* <Resume/> */}
+        <Routes>
+          <Route exact path='/' element={< Resume/>}/>
+
+          <Route exact path='/resume' element={< Resume/>}/>
+
+          <Route path='/skills' element={<Skills/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+
+
+        </Routes>
+      </div>
+      </BrowserRouter>
+   
   );
 }
 
---
+++ src/pages/Resume.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './Resume.css'
+import{Link} from 'react-router-dom'
+
+
+
+class Resume extends React.Component {
+    render(){
+        return(
+        <div className='Resume'>
+            <div class="all">
+                    <div class="l0"></div>
+                    <div class="l1"></div>
+                    <div class="l2"></div>
+                    <div class="l3"></div>
+
+                    <b><Link to="/skills">skills</Link></b>
+                    <b><Link to="/resume">resume</Link></b>
+                    <b><Link to="/contact">contact</Link></b>
+
+
+
+                <h1 className='pr-title'>professional experience</h1>
+                <div class="someword">Lorem ipsum dolor sit amet consectetur. Lorem, ipsum dolor. Lorem, ipsum.</div>
+
+                <div class="maincircle"><i className='star'><FontAwesomeIcon icon="fa fa-star"/></i></div>
+                <div class="circle1"></div>
+                <div class="circle2"></div>
+                <div class="circle3"></div>
+
+
+                <div class="before">2015-2020</div>
+                <div class="after">
+                    UI Designer<br/>
+                    Vivo Senior Designer<br/>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diamanonummy nibh euismod tincidunt ut laoreet dolore magna<br/>
+                    aliquam erat volutpat. Ut wisi enim ad minim veniam, quis';
+                </div>
+
+                <div class="after2">2012-2015</div>
+                <div class="before2">
+                    UI Designer <br/>
+                    Vivo Senior Designer <br/>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diamanonummy nibh euismod tincidunt ut laoreet dolore magna <br/>
+                    aliquam erat volutpat. Ut wisi enim ad minim veniam, quis';
+                </div>
+                
+
+                <div class="before3">
+                2012-2008
+                </div>
+                <div class="after3">
+                    UI Designer <br/>
+                    Vivo Senior Designer <br/>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diamanonummy nibh euismod tincidunt ut laoreet dolore magna <br/>
+                    aliquam erat volutpat. Ut wisi enim ad minim veniam, quis';
+                </div>
+
+            </div>
+        </div>
+                    
+        )
+    }
+}
+export default Resume
