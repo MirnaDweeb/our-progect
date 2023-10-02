992b6cd851e1c5db4ba4f706dfb219c0bdb52c30
shosh0 <smt5557755@gmail.com>
2023-10-02 16:55:15 +0300

    shimaa , skills edit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Contact from './pages/Contact';
+import Skills from './pages/Skills';
+import './pages/skills.css'
+
 function App() {
   return (
    <Contact/>
